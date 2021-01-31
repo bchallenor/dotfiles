7a001a7e4894675f08c8d12be269f081319b1e17
Ben Challenor <ben@challenor.org>
2021-01-31 14:49:30 +0000

    Write straight.el lockfile

---
+++ emacs/.emacs.d/straight/versions/default.el
@@ -0,0 +1,5 @@
+(("emacsmirror-mirror" . "77ca06a6d6d668f6c09faee7179d75c4a8111c6b")
+ ("gnu-elpa-mirror" . "5f07df669edbc0db7695545f6cc2514575d88cb0")
+ ("melpa" . "61f6ede846bbf5e364c626524c567d4e3e99b906")
+ ("straight.el" . "2d407bccd9378f1d5218f8ba2ae85c6be73fbaf1"))
+:beta
